0bbe19a8a49dfa42cb655a45024264d0c58c74c0
Kenney Chan <Kcha582@aucklanduni.ac.nz>
2018-09-22 00:24:55 +1200

    Implement node server. Running works. Implement REST calls for prediction

--- server/server.js
+++ server/server.js
@@ -1,8 +1,51 @@
+const {processImage,toTensor} = require('./classifier.js')
 const express = require('express')
-const cors = require('cors')
+const fs = require('fs')
+const Canvas = require('canvas')
+const tf = require('@tensorflow/tfjs')
+const tfcon = require('@tensorflow/tfjs-converter')
+require('@tensorflow/tfjs-node')
 
-const server = express()
+const port = 8080;
+var model;
 
-server.use(cors())
+/*tfcon.loadFrozenModel('file://./model/tensorflowjs_model.pb','file://./model/weights_manifest.json').then(Fmodel => {
+    model = Fmodel;
+}).catch((err) => {
+    console.log(err)
+})*/
 
-server.use(express.static)
+tf.loadModel('http://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json').then(data =>
+{
+    model = data;
+}).catch(err => console.log(err))
+
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send('Hello from Express!')
+})
+
+app.get('/predict', (request, response) => {
+    const can = new Canvas(244,244);
+    const pixels = tf.fromPixels(can);
+    const dimensions = tf.reshape(pixels,[-1,244,244,3])
+    console.log(model.predict(dimensions));
+})
+
+
+//loadImage().then( data => {console.log(data)}).catch(err => console.log(err))
+
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+})
+
+
+
+
+
